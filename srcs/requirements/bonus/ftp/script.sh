aad225227a414b68815b8c8d479ebffcf13ea225
Lukas Seeger <lseeger@.42heilbronn.de>
2026-01-28 13:55:15 +0100

    vsftpd dir

--- srcs/requirements/bonus/ftp/script.sh
+++ srcs/requirements/bonus/ftp/script.sh
@@ -4,6 +4,9 @@ set -e
 FTP_USER="$(cat /run/secrets/ftp_user)"
 FTP_PASS="$(cat /run/secrets/ftp_password)"
 
+# Create required vsftpd chroot dir
+mkdir -p /var/run/vsftpd/empty
+
 # Create user if not exists
 if ! id "$FTP_USER" >/dev/null 2>&1; then
     useradd -m "$FTP_USER"
